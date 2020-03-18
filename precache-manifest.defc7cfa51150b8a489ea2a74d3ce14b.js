893d1061e87579a49af1220a4e2c12a0f34b242a
David Zuluaga <david_fza@hotmail.com>
2020-03-17 22:20:41 -0500

    Updates

---
+++ precache-manifest.defc7cfa51150b8a489ea2a74d3ce14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10aed41f6a67f107e065ae8be5413bb",
+    "url": "/poke-api-react/index.html"
+  },
+  {
+    "revision": "297eaea698dd04d0baa1",
+    "url": "/poke-api-react/static/css/main.8bb0d546.chunk.css"
+  },
+  {
+    "revision": "2d98525889bcbdc95be3",
+    "url": "/poke-api-react/static/js/2.1d3df887.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/poke-api-react/static/js/2.1d3df887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297eaea698dd04d0baa1",
+    "url": "/poke-api-react/static/js/main.784a7e5b.chunk.js"
+  },
+  {
+    "revision": "c5c26920cd76acd83e36",
+    "url": "/poke-api-react/static/js/runtime-main.79f016fc.js"
+  }
+]);
